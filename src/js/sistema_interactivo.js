0d7477235c4bb17f5c1fbf8daf0c54dc5f2861a4
Sebastian Linero De Castro <sebastianlinero15@gmail.com>
2025-06-19 19:54:01 -0500

    feat: add other files

---
+++ src/js/sistema_interactivo.js
@@ -0,0 +1,7 @@
+// We start by creating our name and age variables that they take my data from the html inputs
+
+const nameUser = document.getElementById("name")
+const ageUser = document.getElementById("age")
+
+const answerUser = document.getElementById("answer")
+answerUser.textContent = 'holaa'
